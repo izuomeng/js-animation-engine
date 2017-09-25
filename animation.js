37cae6dc171d2bb6ed9e68a6fffc2f0432aaf071
izuomeng <dmate@foxmail.com>
2017-09-25 13:07:25 +0800

    animate same like jq

--- animation.js
+++ animation.js
@@ -1,6 +1,7 @@
 let $ = (function() {
     let styleElement,
         animationList = []
+    animationList.busy = false
     function $(element) {
         if(this instanceof $){
             if(typeof element === 'object'){
@@ -13,33 +14,16 @@ let $ = (function() {
     }
     $.prototype = {
         constructor: $,
-        animate(inQueue, cssText, duration = '1s', easing = 'ease', callback = ()=>{}) {
-            // 立即开始执行的动画
-            if(!inQueue){
-                let removeAnimation = () => {
-                    this.el.style.cssText += cssText
-                    this.clearStyle(this.id)
-                    callback()
-                    this.el.removeEventListener('animationend', removeAnimation)
-                }
-                this.el.addEventListener('animationend', removeAnimation)
-                this.id = Math.random().toString().slice(2,6)
-                let pre = getComputedStyle(this.el).animation,
-                    comma = pre.length <= 0 ? '' : `${pre},`,
-                    now = comma + `play${this.id} ${duration} ${easing}`
-                addCSSRule(`.animate${this.id}{animation: ${now};transform: translate3d(0)}`)
-                addCSSRule(`@keyframes play${this.id}{100%{${cssText}}}`)
-                this.el.classList.add(`animate${this.id}`)
-                console.log(getComputedStyle(this.el).animation)
-            }else{
-                animationList.push({    // 加入队列的动画
-                    cssText,
-                    duration,
-                    easing,
-                    callback
-                })
-                return this                
-            }
+        animate(cssText, duration = '1s', easing = 'ease', callback = ()=>{}) {
+            // 加入队列的动画
+            animationList.push({    
+                cssText,
+                duration,
+                easing,
+                callback
+            })
+            this.start()
+            return this                
         },
         animateOnece(cssText, duration = '1s', easing = 'ease', callback = ()=>{}) {
             let removeAnimation = () => {
@@ -47,16 +31,21 @@ let $ = (function() {
                 this.clearStyle(this.id)
                 callback()
                 this.el.removeEventListener('animationend', removeAnimation)
+                animationList.busy = false
             }
             this.el.addEventListener('animationend', removeAnimation)
             this.id = Math.random().toString().slice(2,6)
             addCSSRule(`.animate${this.id}{animation: play${this.id} ${duration} ${easing};transform: translate3d(0)}`)
             addCSSRule(`@keyframes play${this.id}{100%{${cssText}}}`)
             this.el.classList.add(`animate${this.id}`)
+            animationList.busy = true
         },
         start() {
             let len = animationList.length
             if(len <= 0){
+                return
+            }
+            if(animationList.busy){
                 return
             }
             let fireAnimation = () => {
